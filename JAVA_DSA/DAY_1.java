a3c213a9163cade7bd8a9c9ef04ac148e61df69c
Jithin Dev <72137419+jithin317@users.noreply.github.com>
2024-05-28 19:27:57 +0530

    Day-1 Complete

--- JAVA_DSA/DAY_1.java
+++ JAVA_DSA/DAY_1.java
@@ -1,10 +1,37 @@
 package cat_DSA.JAVA_DSA;
 
+// Recursion
 public class DAY_1 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+//		System.out.println(calculatePower(2,4));
 	}
-
+	
+	public static void print1tonNumbers(int n) {
+		if(n == 0) {
+			return;
+		}
+		System.out.println(n);
+		print1tonNumbers(n - 1);
+	}
+	
+	public static int sumofNaturalNumbers(int n) {
+		if(n == 0) {
+			return 0;
+		}
+		return n + sumofNaturalNumbers(n - 1);
+	}
+	
+	public static int sumOfDigits(int n) {
+		if(n % 10 == 0) {
+			return 0;
+		}
+		return n % 10 + sumOfDigits(n / 10);
+	}
+	
+	public static int calculatePower(int num,int pow) {
+		if(pow == 0) {
+			return 1;
+		}
+		return num * calculatePower(num,pow - 1);
+	}
 }
---
+++ JAVA_DSA/Day_2.java
@@ -0,0 +1,11 @@
+package cat_DSA.JAVA_DSA;
+
+// Searching & Sorting
+public class Day_2 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
